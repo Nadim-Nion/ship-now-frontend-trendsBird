955b161998d0c70cc0c37e6e01e3f282d481e731
Nadim-Nion <nadim15-1746@diu.edu.bd>
2026-07-27 16:10:14 +0600

    feat(ui): update main layout, login view, brand assets, and build config

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router";
+import Login from "../../pages/Login";
+
 const MainLayout = () => {
   return (
     <div>
-      <h2>React Project Setup</h2>
-      <p>This is Main Layout</p>
+      {/* <h2>React Project Setup</h2>
+      <p>This is Main Layout</p> */}
+      <Login />
+      <Outlet />
     </div>
   );
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,19 @@
 const Login = () => {
-  return <div>
-    <h2>Login</h2>
-  </div>;
+  return (
+    <div className="grid grid-cols-2">
+      {/* Left Column - Ship Now Introduction */}
+      <div className="w-full h-full border-2 border-orange-500 bg-[#8c8b94]">
+        {/* Image and Text Container */}
+        <div className="flex items-center justify-center">
+          <img src="./src/assets/Ship_Now_Logo.png" alt="Ship Now Logo" className="h-6 w-5" />
+          <p className="text-white text-center mt-4">Ship Now</p>
+        </div>
+      </div>
+
+      {/* Right Column - Login Form */}
+      <div className="w-full h-full border-2 border-red-500">Login Form</div>
+    </div>
+  );
 };
 
 export default Login;
