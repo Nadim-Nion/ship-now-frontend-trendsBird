e07c7e73c6070b80c8afec4d835be2b7e424b1d3
Nadim-Nion <nadim15-1746@diu.edu.bd>
2026-07-28 18:11:15 +0600

    feat: responsive login page for large, medium and small screen

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,67 +2,99 @@ import LoginForm from "../components/form/LoginForm";
 
 const Login = () => {
   return (
-    <div className="grid grid-cols-2">
-      {/* Left Column - Ship Now Introduction */}
-      <div className="w-full h-full bg-[#856DF3]">
-        {/* Image and Text Container */}
-        <div className="flex items-center justify-center mt-24 mb-20">
+    <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2 overflow-hidden">
+      {/* Left Section */}
+      <section className="bg-[#856DF3] flex flex-col items-center justify-center px-6 py-12 md:px-10 lg:px-12 xl:px-16">
+
+        {/* Logo */}
+        <div className="flex items-center justify-center gap-3  lg:mb-20 mb-14">
           <img
             src="./src/assets/Ship_Now_Logo.png"
             alt="Ship Now Logo"
-            className="h-8 w-7"
+            className="w-8 h-8 md:w-10 md:h-10"
           />
-          <p className="text-center mt-4 italic font-black text-3xl text-white uppercase">
+
+          <h2 className="text-white font-black italic uppercase text-3xl md:text-4xl">
             ShipNow
-          </p>
+          </h2>
         </div>
 
-        {/* Double Images Container */}
-        <div className="relative flex items-center justify-center">
+        {/* Images */} {/* h-8 w-7 */}
+        <div className="relative w-fit">
           <img
             src="./src/assets/images/Ship_Now_Image_1.jpg"
-            alt="Ship Now Image 1"
-            className="w-102.5 h-96.5 mt-4 rounded-lg"
+            alt="Ship Now"
+            className="
+            w-[243px]
+            h-[229px]
+            md:w-[410px]
+            md:h-[386px]
+            rounded-2xl
+            "
           />
 
-          {/* Child Image Container */}
-          <div>
-            <img
-              src="./src/assets/images/Ship_Now_Image_2.jpg"
-              alt="Ship Now Image 2"
-              className="w-[178.46px] h-57 mt-4 -top-15 left-110 absolute rounded-lg"
-            />
-          </div>
+          <img
+            src="./src/assets/images/Ship_Now_Image_2.jpg"
+            alt="Ship Now"
+            className="
+            absolute
+            -top-9
+            -right-8
+            w-[105px]
+            h-[135px]
+            md:w-[178px] 
+            md:h-[218px]
+            rounded-2xl
+            shadow-2xl
+            "
+          />
         </div>
 
-        {/* Welcome To Ship Now */}
-        <div className="text-center my-20">
-          <h2 className="text-4xl text-white font-extrabold">
+        {/* Text */}
+        <div className="mt-12 text-center max-w-md">
+          <h1 className="text-white font-bold text-3xl md:text-4xl xl:text-[42px]">
             Welcome To ShipNow
-          </h2>
-          <p className="text-white mt-2">
+          </h1>
+
+          <p className="text-white/80 mt-4 text-sm md:text-base lg:text-lg">
             Manage your shipments, fleet, and warehouse in one smart dashboard.
           </p>
         </div>
-      </div>
+      </section>
 
-      {/* Right Column - Login Form */}
-      <div className="w-3/4 h-1/12 mx-auto my-20">
-        {/* Text Container */}
-        <div className="m-10">
+      {/* Right Section */}
+
+      <section
+        className="
+        flex
+        items-center
+        justify-center
+        bg-white
+        px-6
+        py-12
+        md:px-10
+        lg:px-8
+        xl:px-16
+        "
+      >
+        <div className="w-full max-w-[420px]">
           <img
             src="./src/assets/Ship_Now_Logo_2.png"
-            alt="Ship Now 2nd Logo"
-            className="mx-auto mt-20 w-10 h-10"
+            alt="logo"
+            className="mx-auto h-10 w-10"
           />
-          <h2 className="text-center mt-8 text-3xl font-bold">Welcome Back</h2>
-          <p className="text-center mt-4 text-[#757575] text-sm mb-8">
+
+          <h2 className="text-center mt-8 text-3xl md:text-4xl font-semibold">
+            Welcome Back
+          </h2>
+
+          <p className="text-center text-[#757575] mt-4 mb-10 text-sm md:text-base">
             Log in to continue managing your logistics with ShipNow
           </p>
+
+          <LoginForm />
         </div>
-        {/* Form */}
-        <LoginForm />
-      </div>
+      </section>
     </div>
   );
 };
