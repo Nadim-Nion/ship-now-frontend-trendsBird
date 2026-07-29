79c0d21ec04151c7f9aaa3745c44a56359a86fcc
Nadim-Nion <nadim15-1746@diu.edu.bd>
2026-07-29 21:16:49 +0600

    fix: image path fixed in dashboard sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -2,13 +2,17 @@ import { ChevronDown } from 'lucide-react'
 import NavRow from './NavRow'
 import { NAV_ITEMS, BOTTOM_NAV_ITEMS } from '../../data/navigationData'
 import { COLOR } from '../../theme/tokens'
+import Logo from "../../assets/Ship_Now_Logo_2.png"
 
 const Sidebar = () => {
   return (
     <aside className="w-[220px] min-w-[220px] h-screen bg-[#FFFFFF] border-r border-[#ECECEC] p-[24px] flex flex-col box-border">
       {/* Logo */}
       <div className="flex items-center gap-3 mb-7">
-        <img src="./src/assets/Ship_Now_Logo_2.png" alt="logo" />
+        <img 
+        // src="./src/assets/Ship_Now_Logo_2.png" 
+        src={Logo}
+        alt="logo" />
         <span className="font-bold text-[17px] text-[#111] tracking-[0.2px] italic">
           SHIPNOW
         </span>
